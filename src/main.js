40bad27c6589e8c5b1701200ab2050710dd8eb4b
bqc <6575734@qq.com>
2019-08-29 10:14:00 +0800

    增加移动端调试工具

--- src/main.js
+++ src/main.js
@@ -8,29 +8,24 @@ import "./assets/less/base.less";
 import "./assets/iconfont/iconfont.css";
 // 动画插件
 import "animate.css";
-
+// 公共组件挂载
 import toast from './components/Toast'
 import loading from './components/Loading'
 Vue.use(toast)
 Vue.use(loading)
-
+// axios
 import axios from "axios";
-
-// import 'lib-flexible'
-import VueAwesomeSwiper from "vue-awesome-swiper";
-
-// require styles
-import "swiper/dist/css/swiper.css";
-
-Vue.use(VueAwesomeSwiper /* { default global options } */);
-
 Vue.prototype.$axios = axios;
-
+// 轮播图
+import VueAwesomeSwiper from "vue-awesome-swiper";
+import "swiper/dist/css/swiper.css";
+Vue.use(VueAwesomeSwiper /* { default global options } */ );
+// 开发环境不提示官网地址
 Vue.config.productionTip = false;
-
+// 挂载封装的异步请求
 import api from "./api/install";
 Vue.use(api);
-
+// 图片懒加载
 import VueLazyload from "vue-lazyload";
 import loadingImg from "./assets/images/loadingForProduct.gif";
 Vue.use(VueLazyload, {
@@ -39,6 +34,11 @@ Vue.use(VueLazyload, {
   loading: loadingImg,
   attempt: 1
 });
+// 移动端调试
+import vconsole from "vconsole";
+let vConsole = new vconsole();
+export default vConsole;
+
 
 router.beforeEach((to, from, next) => {
   /* 路由发生变化修改页面title */
